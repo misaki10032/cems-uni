c5e2aef7268a90b072807994ab872ca02c5da4b0
tian <y13619126257@163.com>
2021-06-27 15:15:25 +0800

    2021-06-27 15:16 [tian] [moudles plus]

---
+++ components/myself/infoUpd.vue
@@ -0,0 +1,96 @@
+<template>
+	<view class="content">
+		<view class="from">
+			<view class="input-controls">
+				<input class="input" @input="btnOk" v-model="nparam" type="text" maxlength="32"/>
+			</view>
+		</view>
+		<button class="btn" hover-class="btn-hover" :disabled="btn" @click="confirmBack">
+			确定
+		</button>
+	</view>
+</template>
+
+<script>
+	export default {
+		props: ['param'],
+		data() {
+			return {
+				nparam: "",
+				nid: "",
+				btn: true,
+				user: uni.getStorageSync("loginUser")
+			}
+		},
+		mounted() {
+			this.nparam = this.$props.param.param
+			this.nid = this.$props.param.id
+		},
+		methods: {
+			confirmBack(){
+				if(this.nid == 1){
+					this.user.userName = this.nparam
+				}else if(this.nid == 2){
+					this.user.userDec = this.nparam
+				}
+				else if(this.nid == 3){
+					this.user.userPname = this.nparam
+				}
+				else if(this.nid == 4){
+					var myreg = /^([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+@([a-zA-Z0-9]+[_|\_|\.]?)*[a-zA-Z0-9]+\.[a-zA-Z]{2,3}$/;
+					if(!myreg.test(this.nparam)){
+						uni.showToast({
+							title: "请输入格式正确的邮箱",
+							icon: "none"
+						})
+						return;
+					}
+					this.user.userEmail = this.nparam
+				}
+				else if(this.nid == 5){
+					this.user.userHouse = this.nparam
+				}
+				this.$myRequest({
+					url: "/revUserInfo",
+					data: this.user,
+					method: "POST",
+					success: res => {
+						if(res.data.code == 200){
+							uni.showToast({
+								title: res.data.msg,
+								icon: "success"
+							})
+							setTimeout(() => {
+								uni.switchTab({
+									url:"/pages/home/home"
+								})
+							},1000)
+						}else{
+							uni.showToast({
+								title: res.data.msg,
+								icon: "success"
+							})
+							setTimeout(() => {
+								uni.switchTab({
+									url:"/pages/home/home"
+								})
+							},1000)
+						}
+					}
+				})
+				/* uni.$emit("send",[this.nparam,this.nid])
+				uni.navigateBack({
+					animationDuration: 200,
+					animationType: "zoom-in"
+				}) */
+			},
+			btnOk(){
+				this.btn = false
+			}
+		}
+	}
+</script>
+
+<style>
+	@import url("../../static/css/common.css");
+</style>
---
+++ pages/backPwd/backPwdThird.vue
@@ -0,0 +1,95 @@
+<template>
+	<view class="content">
+		<view class="from">
+			<view class="msg">请设置新的密码。</view>
+			<view class="input-controls">
+				<image class="img" src="/static/login/pwd.png"></image>
+				<input class="input" v-model="password" type="text" maxlength="32" placeholder="重新设置密码" :password="!showPassword" />
+			</view>
+			<view class="input-controls">
+				<image class="img" src="/static/login/pwd-check.png"></image>
+				<input class="input" v-model="repassword" type="text" maxlength="32" placeholder="重新输入设置密码" :password="!showrePassword" />
+			</view>
+		</view>
+
+		<view class="btn" hover-class="btn-hover" @tap="reg">
+			<text>提交</text>
+		</view>
+	</view>
+</template>
+
+<script>
+
+	export default {
+		onLoad() {
+			const user = uni.getStorageSync("user")
+			this.user = user
+		},
+		data() {
+			return {
+				password: '',
+				repassword: '',
+				user: {}
+			};
+		},
+		methods: {
+			reg() {
+				if (this.password.length < 2) {
+					uni.showToast({
+						icon: 'none',
+						title: '密码长度必须2-20位'
+					});
+					return;
+				}
+				if(this.repassword.length == 0){
+					uni.showToast({
+						title: "请输入确认密码",
+						icon: "none"
+					})
+					return;
+				}
+				if (this.repassword != this.password) {
+					uni.showToast({
+						icon: 'none',
+						title: '密码不一致'
+					});
+					return;
+				}
+				this.$myRequest({
+					url: '/updatePwd?id=' + this.user.userId + "&pwd=" + this.password,
+					method: 'GET',
+					dataType: 'json',
+					success: (res) => {
+						console.log(res);
+						if (res.data.code == 200) {
+							uni.showToast({
+								title: res.data.msg,
+								icon: 'none'
+							});
+							setTimeout(function() {
+								uni.redirectTo({
+									url: "/pages/login/login"
+								})
+							}, 1500)
+						} else {
+							uni.showToast({
+								title: "服务器异常，稍后重试",
+								icon: "loading"
+							});
+							setTimeout(function() {
+								uni.redirectTo({
+									url: "/pages/login/login"
+								})
+							}, 1500)
+						}
+					}
+				});
+
+			}
+		}
+	}
+</script>
+
+<style>
+	@import url("../../static/css/common.css");
+</style>
