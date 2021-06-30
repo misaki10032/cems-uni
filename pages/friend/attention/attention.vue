9dc12215d7edf165ff43ecd8032098351b4a605a
blank <1069664381@qq.com>
2021-06-30 09:23:31 +0800

    2021-6-30 9:23 [cxy] [friend]

---
+++ pages/friend/attention/attention.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<uni-row>
+			<uni-col>1</uni-col>
+		</uni-row>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
